173a81e9d1ddedee9971be66a2709ae4fdee92c6
nullmicgo <nullmicog@gmail.com>
2019-07-21 19:32:27 +0800

    update

---
+++ build/precache-manifest.35326fac17b69948e361286c468792ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84ab9008925d00acc9644ca59db8f3df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab2be0489c261dfbc1ca",
+    "url": "/static/css/main.2d0c115c.chunk.css"
+  },
+  {
+    "revision": "47ba806d704b8f4b1ddf",
+    "url": "/static/js/2.90f31e41.chunk.js"
+  },
+  {
+    "revision": "ab2be0489c261dfbc1ca",
+    "url": "/static/js/main.c6b89728.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -17,10 +17,14 @@ class MusicPlayer extends Component {
 
     this.state = {
       show: false,
+      page:0
     };
 
     this.handleShow = this.handleShow.bind(this);
     this.handleClose = this.handleClose.bind(this);
+    this.goToSlide = this.goToSlide.bind(this);
+
+
   }
 
   handleClose= (e) => {
@@ -29,6 +33,15 @@ class MusicPlayer extends Component {
   handleShow= (e) => {
     this.setState({ show: true });
   }
+
+
+  goToSlide= (e, targetValue) => {
+   
+      console.log("goToSlide  >>"+targetValue);
+      this.setState({ page: targetValue });
+
+  }
+
 
   
   componentDidMount() {
@@ -77,34 +90,102 @@ class MusicPlayer extends Component {
     }
 
 
-    const containerStyle ={
+    const loadingContainerStyle ={
       position:'fixed',
       width: '100%',
       height: '120%',
-      backgroundColor: 'red',
+      backgroundColor: 'black',
       top:'-5px',
       backgroundImage: `url(/images/player.png)`,
       backgroundSize: 'cover'
 
     };
 
+    const overTenHoursContainerStyle ={
+      position:'fixed',
+      width: '100%',
+      height: '120%',
+      backgroundColor: 'black',
+      top:'-5px',
+      backgroundSize: 'cover',
+      color: 'white',
+      fontSize:'20px',
+      padding:'50px'
+      
+
+    };
+
+
+    const loadingWithAnimation =   
+    <div style={loadingContainerStyle}>
+    <div className="loadingIconInPlayer">
+    <div className="request-loader">
+    <span><img src="loading.png" onClick={(e) => this.goToSlide(e,1)} /></span>
+  </div>
+</div>
+</div>;
+
+    const overTenHours =   <div style={overTenHoursContainerStyle}>
+        閣下十小時免費使用的療癒時間已經結束, 請問是否需要付費繼續享用療癒時間?
+        <br/> <br/> <br/> <br/> <br/>
+        <button type="submit" className="payment-no-button ">否</button>
+        <button type="submit" className="payment-yes-button " onClick={(e) => this.goToSlide(e,2)}>是</button>
+
+    </div>;
+
+
+    const paymentScreen =   <div style={overTenHoursContainerStyle}>
+    <div className="payment-text">繼續享用療癒時間每分鐘只需US$1.00</div>
+    <br/> <br/> <br/> <br/> <br/>
+    <img src="visa.png" className="payment-image"></img>
+    <button type="submit" className="payment-confirm-button" onClick={(e) => this.goToSlide(e,3)}>繼續</button>
+
+    </div>;
+
+
+const inputGreatSaying =   <div style={overTenHoursContainerStyle}>
+<div className="payment-text">請輸入勵志金句後繼續播放</div>
+<br/> <br/> 
+<input type="text" className="payment-input" ></input>
+<br/> <br/> <br/>
+<button type="submit" className="payment-confirm-button" onClick={(e) => this.goToSlide(e,4)}>繼續</button>
+
+</div>;
+
+
+
+const birdSinging =   
+<div style={loadingContainerStyle}>
+<div className="loadingIconInPlayer">
+<div className="request-loader">
+<span>鳥叫聲</span>
+</div>
+</div>
+</div>;
+
+    let finalShowingContent= "";
+    if(this.state.page === 0 ){
+      finalShowingContent = loadingWithAnimation;
+    }else if(this.state.page === 1 ){
+      finalShowingContent = overTenHours;
+    }else if(this.state.page === 2 ){
+      finalShowingContent = paymentScreen;
+    }else if(this.state.page === 3 ){
+      finalShowingContent = inputGreatSaying;
+    }else if(this.state.page === 4 ){
+      finalShowingContent = birdSinging;
+    }
+
     return (
 
 
 
 
-      <main style={containerStyle}>
+      <main >
 
 
-
-          <div className="loadingIconInPlayer">
-
-          <div class="request-loader">
-          <span><img src="loading.png" /></span>
-        </div>
-
-
-          </div>
+          {finalShowingContent}
+        
 
 
       </main>
